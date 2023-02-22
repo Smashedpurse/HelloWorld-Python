8cbba2fc42a8fe678dab574465a1af29bf175dd8
Ivan Rubio <smashedpurse@gmail.com>
2023-02-22 14:18:45 -0600

    Add a Multilince Comment

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
-#Hola mundo, este es un comentario
+#Hola mundo, este es un comentario.
+#Nuestro hola mundo en Python.
 print("Hola Python") 
+
+
+"""
+This is multiline comment
+multiline comment takes multiple lines.
+python is eating the world
+"""
