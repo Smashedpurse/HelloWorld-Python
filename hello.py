c66b3376078da85ebb61180fa0b52cb1ab709605
Ivan Rubio <smashedpurse@gmail.com>
2023-02-22 14:24:58 -0600

    Add a second of a string using simple quote

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-#Hola mundo, este es un comentario.
-#Nuestro hola mundo en Python.
-print("Hola Python") 
-
-
+#Hello World, this is a comment.
+#Hi everyone, this is a second comment.
+print("Hola Python")
+print('Hello World')
 """
 This is multiline comment
 multiline comment takes multiple lines.
 python is eating the world
 """
+
