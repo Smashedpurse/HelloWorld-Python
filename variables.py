b1e6aff79bed1820daf4d9c06a596ec760417b35
Ivan Rubio <smashedpurse@gmail.com>
2023-02-23 13:17:53 -0600

    Use the function str to convert and int to a string

--- variables.py
+++ variables.py
@@ -3,7 +3,12 @@
 #In Python we declare a variable only in lower case and snake case
 
 myvaraible = ("My String Variable") # Lower Case
-print(myvaraible)
-
 my_second_variable=("This is a second variable") #Snake Case
-print(my_second_variable)
+
+mi_int_variable = 5
+
+mi_int_to_str = str(mi_int_variable)
+
+print(type(mi_int_to_str))
+
+
